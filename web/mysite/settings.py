820649683b491d81a809d653b86dd956a2907c32
Michal Broucek <michal.broucek@siemens.com>
2026-01-03 08:12:29 +0100

    Allow hosting on pythonanywhere

--- web/mysite/settings.py
+++ web/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_@@f9fx@m++8)e28)hu079_ty$9x$_h%#l!oea*(5k7stap6yp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mbrouk.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
