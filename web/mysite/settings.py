331ad34224d2f34e81f768d81cd866319516471a
mb256 <mb256@gmail.com>
2026-01-18 17:11:14 +0100

    Move static files into app subfolder

--- web/mysite/settings.py
+++ web/mysite/settings.py
@@ -129,4 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
