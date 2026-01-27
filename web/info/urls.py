402bde5fc3e0dc5394892a3ec99637dd0b963fe3
mb256 <mb256@gmail.com>
2026-01-27 21:17:26 +0100

    Change default page

--- web/info/urls.py
+++ web/info/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("info/", views.info, name="info"),
+    path("index/", views.index, name="index"),
+    path("", views.info, name="info"),
 ]
